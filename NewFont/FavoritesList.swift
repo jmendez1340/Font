b5fb5d7a513f20392aa9bb7371b89d5d5e17f922
Jason <jason@family-iMac.local>
2017-03-12 01:17:52 -0600

    Initial Commit

---
+++ NewFont/FavoritesList.swift
@@ -0,0 +1,48 @@
+//
+//  FavoritesList.swift
+//  NewFont
+//
+//  Created by Jason Mendez on 3/8/17.
+//  Copyright © 2017 Jason Mendez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesList {
+    static let sharedFavoritesList = FavoritesList()
+    private (set) var favorites:[String]
+    
+    init() {
+        let defaults = UserDefaults.standard
+        let storedFavorites = defaults.object(forKey: "favorites") as? [String]
+        favorites = storedFavorites != nil ? storedFavorites! : []
+    }
+    
+    func addFavorite(fontName:String) {
+        if !favorites.contains(fontName) {
+            favorites.append(fontName)
+            saveFavorites()
+        }
+    }
+    func removeFavorite(fontName: String) {
+        if let index = favorites.index(of: fontName) {
+            favorites.remove(at: index)
+            saveFavorites()
+        }
+    }
+    func moveItem(fromIndex from: Int, toIndex to: Int) {
+        let item = favorites[from]
+        favorites.remove(at: from)
+        favorites.insert(item, at: to)
+        saveFavorites()
+    }
+    
+    
+    private func saveFavorites(){
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        defaults.synchronize()
+    }
+    
+}
